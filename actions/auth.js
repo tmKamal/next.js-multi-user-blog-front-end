3fca39336f09eaeed146c4c062c7863cadac692e
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-06-12 01:06:19 +0530

    auth pages - admin, user

---
+++ actions/auth.js
@@ -0,0 +1,106 @@
+import fetch from 'isomorphic-fetch';
+import {API} from '../config';
+import cookie from 'js-cookie';
+export const SignupAction=(user)=>{
+    return fetch(`${API}/signup`,{
+        method:'POST',
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(user)
+    }).then((response)=>{
+        return response.json();
+    }).catch(err=>console.log(err))
+}
+
+export  const SigninAction=(user)=>{
+    return fetch(`${API}/login`,{
+        method:'POST',
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(user)
+    }).then((response)=>{
+        return response.json();
+    }).catch(err=>console.log(err))
+}
+export const SignOut=next=>{
+    removeCookie('token');
+    removeLocalStorage('user');
+    next();
+    return fetch(`${API}/signout`,{
+        method:'GET'
+    }).then(respone=>{
+        console.log('signout success');
+    }).catch(err=>{
+        console.log(err);
+    })
+}
+
+/* Set Cookie
+===============
+
+Next Js runs in the client side and browser side to.. (Server Rendering)
+To set a cookie, first we have to check where we are right now.
+->process.browser
+*/
+export const setCookie=(key,value)=>{
+    var inTwoMinutes = new Date(new Date().getTime() + 2 * 60 * 1000);
+    if(process.browser){
+        cookie.set(key,value,{
+            expires:inTwoMinutes,
+
+        },{ sameSite: 'none' },{   secure: true });
+    }
+};
+/* Remove Cookie - to remove  a cookie we only need the name of the cookie */
+export const removeCookie=(key)=>{
+    if(process.browser){
+        cookie.remove(key,{
+            expires:1
+        })
+    }
+}
+/* Get cookie */
+export const getCookie=(key)=>{
+    if(process.browser){
+       return cookie.get(key)
+    }
+}
+//local storage
+export const setLocalStorage=(key,value)=>{
+    if(process.browser){
+        localStorage.setItem(key,JSON.stringify(value))
+    }
+}
+export const removeLocalStorage=(key)=>{
+    if(process.browser){
+        localStorage.removeItem(key);
+    }
+}
+//authenticate user by pass data to cookie and local storage
+export const authenticate=(data,next)=>{
+    setCookie('token',data.token);
+    setLocalStorage('user',data.user);
+    next();
+
+};//this is act as a middleware
+
+export const isAuth=()=>{
+    if(process.browser){
+        const cookieChecked=getCookie('token');
+        if(cookieChecked){
+            if(localStorage.getItem('user')){
+                console.log('how man how!!');
+                return JSON.parse(localStorage.getItem('user'));//we have stored the data as the json first place. so we have to parse.
+
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../../src/ProTip';
+import Link from '../../src/Link';
+import Copyright from '../../src/Copyright';
+import Navigation from '../../components/navigation';
+import StickyFooter from '../../components/StickyFooter';
+
+
+
+export default function Index() {
+  return (
+    <React.Fragment>
+
+      <Navigation></Navigation>
+      <Box mx={2}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Admin
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <br></br>
+        <Link href="/about" color="secondary">
+          Go to the Signin page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+      <StickyFooter></StickyFooter>
+    </React.Fragment>
+   
+  );
+}
