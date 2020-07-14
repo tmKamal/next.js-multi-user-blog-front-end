a3225d78831205cf0355f8135b4c0a87441d67e8
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-07-15 05:19:50 +0530

    updated the production and dev url of the REST API

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://yourdomain.com"
-  : "https://bloggler.herokuapp.com/api";
+  ? "https://bloggler.herokuapp.com/api"
+  : "http://localhost:8000/api";
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
