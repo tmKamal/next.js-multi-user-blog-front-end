0e2164592e5e9df28fc9c7f827d0e02e5a71c4c8
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-07-15 04:30:53 +0530

    env variables changes

--- config.js
+++ config.js
@@ -4,10 +4,15 @@ const { publicRuntimeConfig } = getConfig();
 export const API = publicRuntimeConfig.PRODUCTION
   ? "https://yourdomain.com"
   : "http://localhost:8000/api";
-  
+
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.DOMAIN_PRODUCTION
   : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
-export const FB_APP_ID=publicRuntimeConfig.FB_APP_ID;
+
+//export const IMG=publicRuntimeConfig.IMG;
+export const IMG = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.PRODUCTION_IMG
+  : publicRuntimeConfig.DEV_IMG;
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 const withCSS=require('@zeit/next-css');
 
 module.exports=withCSS({
+    env:{
+        //can be access using process.env 
+    },
+    serverRuntimeConfig:{
+        //will only be available on the server side
+    },
     publicRuntimeConfig:{
+        //will be available on both server and client
         APP_NAME:'SEOBLOG',
         API_DEVELOPMENT:'http://localhost:8000/api',
-        PRODUCTION:false,
+        PRODUCTION:process.env.PRODUCTION || false,
         DOMAIN_DEVELOPMENT:'http://localhost:3000',
         DOMAIN_PRODUCTION:'http://yourdomain.com',
-        FB_APP_ID:'6987fsak'
+        FB_APP_ID:'6987fsak',
+        DEV_IMG:'http://localhost:8000',
+        PRODUCTION_IMG:'http://YOURDOMAIN'
     }
 });
