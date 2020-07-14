5e1c152331c8abba5346d26eecb4a9753bf5eadb
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-07-15 05:12:51 +0530

    live api url added to the .env variables

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
   ? "https://yourdomain.com"
-  : "http://localhost:8000/api";
+  : "https://bloggler.herokuapp.com/api";
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
