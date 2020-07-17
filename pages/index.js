ce20c013246549767ea9629e8796dabd32fb5183
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-07-18 02:21:37 +0530

    responsive typgraphy css breakpoints

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,37 @@ import Copyright from "../src/Copyright";
 import Navigation from "../components/Navigation";
 import StickyFooter from "../components/StickyFooter";
 import { Button, Grid } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme();
+
+theme.typography.h2 = {
+  fontSize: "2rem",
+  "@media (min-width:600px)": {
+    fontSize: "3rem",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "4rem",
+    fontWeight:'400'
+  },
+};
 
 export default function Index() {
   return (
     <React.Fragment>
       <Navigation></Navigation>
       <Box mx={2} my={5}>
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="textPrimary"
-          gutterBottom
-        >
-          Under Construction
-        </Typography>
+        <ThemeProvider theme={theme}>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Under Construction
+          </Typography>
+        </ThemeProvider>
         <Typography variant="h5" align="center" color="textSecondary" paragraph>
           It's fun to peek into other people's worlds and see how they go about
           doing things.
@@ -30,14 +46,14 @@ export default function Index() {
           <Grid container spacing={2} justify="center">
             <Grid item>
               <Link href="/blog">
-                <Button  variant="contained" color="primary">
+                <Button variant="contained" color="primary">
                   Take a Peek
                 </Button>
               </Link>
             </Grid>
             <Grid item>
               <Link href="/admin">
-                <Button  variant="outlined" color="primary">
+                <Button variant="outlined" color="primary">
                   Admin Panel
                 </Button>
               </Link>
